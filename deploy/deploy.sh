4c506faa8c03678e9993b866cf47241de0b8d778
한재원 <hanjo8813@gmail.com>
2021-11-22 06:47:28 +0900

    test : 쉘 스크립트 실행

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,6 @@
 ##!/bin/bash
-#
-#DOCKER_USERNAME=$1
-#DOCKER_REPOSITORY=$2
-#
-##cd /srv/
-#
-#docker run -d --name spring_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
+
+DOCKER_USERNAME=$1
+DOCKER_REPOSITORY=$2
+
+docker run -d --name spring_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
