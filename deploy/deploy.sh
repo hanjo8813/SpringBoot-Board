bc1e1fa2f5b3d27bc747d048d65472a7c25c09ec
한재원 <hanjo8813@gmail.com>
2021-11-22 06:42:34 +0900

    test : scp-action 적용

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
-DOCKER_USERNAME=$1
-DOCKER_REPOSITORY=$2
-
-#cd /srv/
-
-docker run -d --name spring_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
+##!/bin/bash
+#
+#DOCKER_USERNAME=$1
+#DOCKER_REPOSITORY=$2
+#
+##cd /srv/
+#
+#docker run -d --name spring_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
