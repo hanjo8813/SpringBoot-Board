54edf6e410175cb86127b14c176debe5a9b032e3
한재원 <hanjo8813@gmail.com>
2021-11-25 06:41:28 +0900

    test : 슬랙 연동 테스트

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,19 +4,19 @@ EXIST_BLUE=$(docker ps | grep blue)
 
 ## 만약 blue(8081)가 구동중이 아니라면? : blue(8081)를 실행 -> green(8082)은 종료
 if [ -z "$EXIST_BLUE" ]; then
-    echo "RUN BLUE"
+    echo "@@@@@@@@@@@@@@@@@@@@@@ RUN BLUE @@@@@@@@@@@@@@@@@@@@@@"
     docker-compose -p board-blue -f docker-compose.blue.yml up -d
 
     sleep 60
 
-    echo "DOWN GREEN"
+    echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN GREEN @@@@@@@@@@@@@@@@@@@@@@"
     docker-compose -p board-green -f docker-compose.green.yml down
 else
-    echo "RUN GREEN"
+    echo "@@@@@@@@@@@@@@@@@@@@@@ RUN GREEN @@@@@@@@@@@@@@@@@@@@@@"
     docker-compose -p board-green -f docker-compose.green.yml up -d
 
     sleep 60
 
-    echo "DOWN BLUE"
+    echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN BLUE @@@@@@@@@@@@@@@@@@@@@@"
     docker-compose -p board-blue -f docker-compose.blue.yml down
 fi
