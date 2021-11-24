f31a64ec39bf7317c6cdd266d01e46316ce984bc
한재원 <hanjo8813@gmail.com>
2021-11-25 06:19:11 +0900

    test : 무중단 배포 테스트

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,28 +20,3 @@ else
     echo "DOWN BLUE"
     docker-compose -p board-blue -f docker-compose.blue.yml down
 fi
-
-
-# 도커만 사용했을 때
-#IS_BLUE_OPEN=$(docker ps | grep spring_con_blue)
-#
-## 만약 blue(8081)가 구동중이 아니라면? : blue(8081)를 실행 -> green(8082)은 종료
-#if [ -z "$IS_BLUE_OPEN"];then
-#  echo "Run blue(8081)"
-#  docker run -d --name spring_con_blue -p 8081:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
-#
-#  sleep 60
-#
-#  echo "Kill green(8082)"
-#  docker stop spring_con_green
-#  docker rm spring_con_green
-#else
-#  echo "Run green(8082)"
-#  docker run -d --name spring_con_green -p 8082:8080 -e "SPRING_PROFILES_ACTIVE=prod" $DOCKER_USERNAME/$DOCKER_REPOSITORY:spring_img
-#
-#  sleep 60
-#
-#  echo "Kill blue(8081)"
-#  docker stop spring_con_blue
-#  docker rm spring_con_blue
-#fi
