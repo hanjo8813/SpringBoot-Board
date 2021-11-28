0912a2e26082907cc54d9e2da759e0a6724fea88
한재원 <hanjo8813@gmail.com>
2021-11-29 01:49:58 +0900

    test : 배포 스크립트 수정

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
 
-# docker build -t hanjo8813/board:spring_img .
-# docker push hanjo8813/board:spring_img
-# docker run -d --name spring_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" hanjo8813/board:spring_img
+# docker build -t temp_img .
+# docker run -d --name temp_con -p 8080:8080 -e "SPRING_PROFILES_ACTIVE=prod" -e "DB_URL=jdbc:mysql://devcourse.cxtvn8tlpabr.ap-northeast-2.rds.amazonaws.com/board_mission" -e "DB_USERNAME=root" -e "DB_PASSWORD=prgrms1234" temp_img
+
+
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,22 +1,41 @@
 ###!/bin/bash
 
-EXIST_BLUE=$(docker ps | grep blue)
+# GitHub에 등록한 환경변수 받아오기
+DOCKER_USERNAME=$1
+DOCKER_REPOSITORY=$2
+DOCKER_TAG=$3
+DB_URL=$4
+DB_USERNAME=$5
+DB_PASSWORD=$6
+
+# 환경변수로 docker-compose 전체용 .env 파일 생성
+echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\nDOCKER_TAG=$DOCKER_TAG" > .env
+
+# docker-compose spring 컨테이너용 DB.env 파일 생성
+echo -e "DB_URL=$DB_URL\nDB_USERNAME=$DB_USERNAME\nDB_PASSWORD=$DB_PASSWORD" > DB.env
+
+# 기존 이미지는 삭제 + 빌드된 최신 이미지 pull
+docker rmi $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
+docker pull $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
+sleep 20
 
 ## 만약 blue(8081)가 구동중이 아니라면? : blue(8081)를 실행 -> green(8082)은 종료
+EXIST_BLUE=$(docker ps | grep blue)
+
 if [ -z "$EXIST_BLUE" ]; then
     echo "@@@@@@@@@@@@@@@@@@@@@@ RUN BLUE @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-blue -f docker-compose.blue.yml up -d
+    docker-compose -p compose_blue -f docker-compose.blue.yml up -d
 
     sleep 60
 
     echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN GREEN @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-green -f docker-compose.green.yml down
+    docker-compose -p compose_green -f docker-compose.green.yml down
 else
     echo "@@@@@@@@@@@@@@@@@@@@@@ RUN GREEN @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-green -f docker-compose.green.yml up -d
+    docker-compose -p compose_green -f docker-compose.green.yml up -d
 
     sleep 60
 
     echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN BLUE @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-blue -f docker-compose.blue.yml down
+    docker-compose -p compose_blue -f docker-compose.blue.yml down
 fi
