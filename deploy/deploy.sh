42bde47997fa48e55f07fedb7fc3478094ebcc44
한재원 <hanjo8813@gmail.com>
2021-11-29 02:05:51 +0900

    test : 배포 테스트

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,12 +9,15 @@ DB_USERNAME=$5
 DB_PASSWORD=$6
 
 # 환경변수로 docker-compose 전체용 .env 파일 생성
+echo "================== UPDATE '.env' file =================="
 echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\nDOCKER_TAG=$DOCKER_TAG" > .env
 
 # docker-compose spring 컨테이너용 DB.env 파일 생성
+echo "================== UPDATE 'DB.env' file =================="
 echo -e "DB_URL=$DB_URL\nDB_USERNAME=$DB_USERNAME\nDB_PASSWORD=$DB_PASSWORD" > DB.env
 
 # 기존 이미지는 삭제 + 빌드된 최신 이미지 pull
+echo "================== UPDATE docker image =================="
 docker rmi $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
 docker pull $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
 sleep 20
@@ -23,19 +26,19 @@ sleep 20
 EXIST_BLUE=$(docker ps | grep blue)
 
 if [ -z "$EXIST_BLUE" ]; then
-    echo "@@@@@@@@@@@@@@@@@@@@@@ RUN BLUE @@@@@@@@@@@@@@@@@@@@@@"
+    echo "================== RUN BLUE =================="
     docker-compose -p compose_blue -f docker-compose.blue.yml up -d
 
     sleep 60
 
-    echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN GREEN @@@@@@@@@@@@@@@@@@@@@@"
+    echo "================== DOWN GREEN =================="
     docker-compose -p compose_green -f docker-compose.green.yml down
 else
-    echo "@@@@@@@@@@@@@@@@@@@@@@ RUN GREEN @@@@@@@@@@@@@@@@@@@@@@"
+    echo "================== RUN GREEN =================="
     docker-compose -p compose_green -f docker-compose.green.yml up -d
 
     sleep 60
 
-    echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN BLUE @@@@@@@@@@@@@@@@@@@@@@"
+    echo "================== DOWN BLUE =================="
     docker-compose -p compose_blue -f docker-compose.blue.yml down
 fi
